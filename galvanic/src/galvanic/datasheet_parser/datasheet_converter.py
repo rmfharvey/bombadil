e0a0cca4de925be5150650afcab858e24d4d3507
Ross Harvey <rmfharvey@gmail.com>
2025-05-09 22:05:42 -0700

    Reran datashert parser on TPS92520

--- galvanic/src/galvanic/datasheet_parser/datasheet_converter.py
+++ galvanic/src/galvanic/datasheet_parser/datasheet_converter.py
@@ -56,11 +56,11 @@ class DatasheetConverter:
 
     def analyze_datasheet(self):
         # AI Model Calls
-        # self._json["high_level"] = self.AI_PARSER_get_high_level_info()
-        # self._json["pins"] = self.AI_PARSER_get_pin_info()
-        #
-        # if self.has_serial_bus:
-        self._json["serial_bus"] = self.AI_PARSER_get_comms_register_map()
+        self._json["high_level"] = self.AI_PARSER_get_high_level_info()
+        self._json["pins"] = self.AI_PARSER_get_pin_info()
+
+        if self.has_serial_bus:
+            self._json["serial_bus"] = self.AI_PARSER_get_comms_register_map()
 
     def load_source(self, source_path):
         ### Get source file, based on input type
