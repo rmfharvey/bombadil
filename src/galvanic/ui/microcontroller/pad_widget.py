a3cca0a96f78c6d095afe1dc0c925b30051f2f81
Ross Harvey <rmfharvey@gmail.com>
2025-12-26 10:33:34 -0600

    Fixed imports and added claude.md file

--- src/galvanic/ui/microcontroller/pad_widget.py
+++ src/galvanic/ui/microcontroller/pad_widget.py
@@ -1,13 +1,14 @@
+from __future__ import annotations  # Skip typehinting evaluation
 from galvanic.ui.microcontroller.forms.pad_ui import Ui_Form as PadForm
 from galvanic.ui import GWidget
 from typing import TYPE_CHECKING
 
-# if TYPE_CHECKING:
-#     from galvanic_design.microcontroller.pad import Pad
+if TYPE_CHECKING:  # Avoid circular dependency with Pad/PadWidget
+    from galvanic_design.microcontroller.pad import Pad
 
 
 class PadWidget(GWidget):
-    def __init__(self, pad_object):
+    def __init__(self, pad_object: Pad):
         super().__init__(form=PadForm, linked_obj=pad_object)
         self._setup()
 
